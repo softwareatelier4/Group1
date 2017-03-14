9298fba1ab83adf24dae1cc552660e8df24082d2
Luca Dolfi <ldolfi@gmail.com>
2017-03-14 15:44:23 +0100

    creating test for creation of freelance

--- test/models/constructAndCreate/Freelance.js
+++ test/models/constructAndCreate/Freelance.js
@@ -13,7 +13,7 @@ var projectRoot = config.projectRoot;
 require(projectRoot+ '/models/Freelance');
 require(projectRoot + '/models/Review');
 
-describe('Freelance Construction and creation', function(done){
+describe('Freelance Construction and creation\n', function(done){
 
   // Freelance constructor test
   describe('Freelance model definition', function(){
@@ -32,6 +32,23 @@ describe('Freelance Construction and creation', function(done){
 
   describe('When creating a new freelance', function(done){
     var Freelance = mongoose.model('Freelance');
+    var freelance;
+
+    before(function(done){
+      //connect and drop db
+      utils.connectAndDropDb(function(err){
+        if (err) return done(err);
+
+        freelance = new Freelance({
+          name : 'Mark Knopfler',
+          email : 'ripperoni@pepe.pe'
+        });
+        
+        freelance.save(function(err,save){
+          if (err) return done(err);
+        });
+      });
+    });
   });
 });
 
