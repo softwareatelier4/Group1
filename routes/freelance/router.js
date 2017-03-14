7b009d375a19606a7289f1f49ce0d2282aa83373
Patrick Balestra <me@patrickbalestra.com>
2017-03-14 15:59:42 +0100

    Add POST review route

--- routes/freelance/router.js
+++ routes/freelance/router.js
@@ -8,12 +8,13 @@ const utils = require('../utils');
 var mongoose = require('mongoose');
 var ObjectId = mongoose.Types.ObjectId;
 const Freelance = mongoose.model('Freelance');
+const Review = mongoose.model('Review');
 
 // Supported methods.
 router.all('/', middleware.supportedMethods('GET, POST, PUT, OPTIONS'));
 router.all('/:freelanceid', middleware.supportedMethods('GET, PUT, OPTIONS')); //add delete later
 
-// GET:freelanceid
+// GET freelance/:freelanceid
 router.get('/:freelanceid', function(req, res, next) {
   if (ObjectId.isValid(req.params.freelanceid)) {
     Freelance.findById(req.params.freelanceid).lean().exec(function(err, freelance){
@@ -35,7 +36,18 @@ router.get('/:freelanceid', function(req, res, next) {
   else res.sendStatus(400);
 });
 
-// POST
+// POST freelance/:freelanceid/review
+router.post('/:freelanceid/review', function(req, res, next) {
+  const newReview = new Review(req.body);
+  newReview.save(function(err, saved) {
+    if (err) {
+      res.status(400).json(utils.formatErrorMessage(err));
+    }
+    res.status(201).json(saved);
+  })
+});
+
+// POST freelance/
 router.post('/', function(req, res, next) {
   const newFreelance = new Freelance(req.body);
   newFreelance.save(function(err, saved) {
