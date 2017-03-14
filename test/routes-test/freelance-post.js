24195de3688658f44e659b7a408b7dffffd21d79
SusannaRiccardi <susi.96@hotmail.it>
2017-03-14 14:30:44 +0100

    Added tests for POST /freelance.

--- test/routes-test/freelance-post.js
+++ test/routes-test/freelance-post.js
@@ -5,15 +5,53 @@ var config = require('../config');
 var app = require(config.projectRoot + '/app');
 var request = require('supertest');
 
-describe('Placeholder test: ', function() {
-  
-  it('app should get answer 200 on GET /', function(done) {
-  	request(app)
-  	  .get('/')
-  	  .expect(200)
-  	  .end(function(err,res) {
-  	    if (err) done(err);
-  	    else done();
-  	  });
+describe('Freelance-post test: ', function() {
+  // TEST: correct post.
+  it('app should get answer 200 on POST /freelance', function(done) {
+    request(app)
+    .post('/freelance')
+    .send({"name" : "Patrick Balestra", "email" : "user@email.com"})
+    .expect(200)
+    .end(function(err, res) {
+      if (err) {
+        done(err);
+      } else {
+        res.body.should.have.property("name", "Patrick Balestra");
+        res.body.should.have.property("email", "user@email.com");
+        done();
+      }
+    });
+  });
+
+  // TEST: not correct post: path "name" is required.
+  it('app should get answer 400 on POST /freelance', function(done) {
+    request(app)
+    .post('/freelance')
+    .send({"email" : "user@email.com"})
+    .expect(400)
+    .end(function(err, res) {
+      if (err) {
+        done(err);
+      } else {
+        res.should.have.property("text", "{\"reason\":\"Freelance validation failed\",\"errors\":[\"Path `name` is required.\"]}")
+        done();
+      }
+    });
+  });
+
+  // TEST: not correct post: path "email" is required.
+  it('app should get answer 400 on POST /freelance', function(done) {
+    request(app)
+    .post('/freelance')
+    .send({"name" : "Patrick Balestra"})
+    .expect(400)
+    .end(function(err, res) {
+      if (err) {
+        done(err);
+      } else {
+        res.should.have.property("text", "{\"reason\":\"Freelance validation failed\",\"errors\":[\"Path `email` is required.\"]}")
+        done();
+      }
+    });
   });
 });
