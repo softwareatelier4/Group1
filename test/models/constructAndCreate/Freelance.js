1b8e0750914822116f815bff3f7ae75c691c1f78
Luca Dolfi <ldolfi@gmail.com>
2017-03-14 15:07:03 +0100

    added utils file for tests on models

--- test/models/constructAndCreate/Freelance.js
+++ test/models/constructAndCreate/Freelance.js
@@ -1,1 +1,38 @@
 /* tests for constructor and creation of Freelance */
+
+//connect to DB
+const mongoose   = require('mongoose');
+mongoose.Promise = require('bluebird'); 
+
+const should = require('should');
+const utils = require('../utils');
+const config = require('../../config');
+
+var projectRoot = config.projectRoot;
+
+require(projectRoot+ '/models/Freelance');
+require(projectRoot + '/models/Review');
+
+describe('Freelance Construction and creation', function(done){
+
+  // Freelance constructor test
+  describe('Freelance model definition', function(){
+    it('should have a constructor', function(){
+      var Freelance;
+      try{
+        Freelance = mongoose.model('Freelance');
+      }catch(err){
+        console.log(err.stack);
+      }finally{
+        should.exist(Freelance, 'expected Freelance constructor to exist');
+        Freelance.should.be.a.Function;
+      }
+    })
+  });
+
+  describe('When creating a new freelance', function(done){
+    var Freelance = mongoose.model('Freelance');
+  });
+});
+
+
---
+++ test/models/utils.js
@@ -0,0 +1,46 @@
+/* Utilities for the models tests */
+/* Taken from "wa-exercise-5-2016-2017" by "Vincenzo Ferme, Andrea Gallidabino, Vasileios Triglianos" */
+
+var config = require('../config')
+var should = require('should');
+var mongoose = require('mongoose');
+var ObjectId = mongoose.Types.ObjectId;
+
+/** 
+* Connects to mongo and drops the database.
+*/
+module.exports.connectAndDropDb = function connectAndDropDb(done){
+  //check if connection has opened but is not ready yet
+  if (mongoose.connection && mongoose.connection.readyState ==2){
+    mongoose.createConnection(config.mongoUrl + config.mongoDbName, function(err){
+      if(err) return done(err);
+      dropDb(done);
+    });
+  }else{
+    mongoose.connect(config.mongoUrl + config.mongoDbName, function(err){
+      if (err) {
+        //if connection is already open it's fine
+        if(err.message !== 'Trying to open unclosed connection.'){
+          return done(err);
+        }
+      }
+      dropDb(done);
+    });
+  } 
+}
+
+/** 
+* Drops the database and closes the mongoose connection.
+*/
+module.exports.dropDbAndCloseConnection = function dropDbAndCloseConnection(done){
+  //drop database
+  mongoose.connection.db.dropDatabase( function(err){
+    if(err) return done(err);
+
+    //close connection
+    mongoose.connection.close( function(err){
+      if(err) return done(err);
+      done();
+    });
+  });
+}
