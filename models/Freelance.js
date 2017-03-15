a5096f0edc87a4ed3b11a9c99c5f3e42ea5c2953
falcon <aron.fiechter@gmail.com>
2017-03-15 01:50:11 +0100

    Readded versionKey false to Freelance model.

--- models/Freelance.js
+++ models/Freelance.js
@@ -28,7 +28,11 @@ const Freelance = exports.Freelance = new mongoose.Schema({
 		avgScore 		: { type: Number },
 		reviews			: [{ type: ObjectID, ref: "Review", default: [] }],
 		tags			: [{ type: String, default: [] }],
-});
+	},
+	{
+		versionKey	: false,
+	}
+);
 
 
 Freelance.pre('save', function (next) {
