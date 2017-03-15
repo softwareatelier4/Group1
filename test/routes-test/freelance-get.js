40acc083bda2ca62038db627bb5d26e11ace706c
falcon <aron.fiechter@gmail.com>
2017-03-15 02:29:09 +0100

    Added moar tests for freelance-get.

--- test/routes-test/freelance-get.js
+++ test/routes-test/freelance-get.js
@@ -18,7 +18,7 @@ describe('Freelance-get test: ', function() {
     before(seed);
     after(utils.dropDb);
 
-    it('should list the freelance with correct data', function(done){
+    it('should list the freelance with correct data', function(done) {
       request(app)
         .get('/freelance/' + seedData[0].data[0]._id.toString())
         .set('Accept', 'application/json')
@@ -31,13 +31,34 @@ describe('Freelance-get test: ', function() {
         });
     });
 
+    it('should respond with a 404 if the freelance does not exist', function(done) {
+      request(app)
+        .get('/freelance/' + ObjectId().toString())
+        .set('Accept', 'application/json')
+        .expect(404, done);
+    });
+
+    it('should respond with a 400 if the given freelance ID is not valid', function(done) {
+      request(app)
+        .get('/freelance/' + 'literally_anything_which_is_not_in_the_correct_format')
+        .set('Accept', 'application/json')
+        .expect(400, done);
+    });
+
+    it('should respond with a 405 if the method is not GET, PUT, or OPTIONS', function(done) {
+      request(app)
+        .head('/freelance/' + seedData[0].data[0]._id.toString()) // valid id
+        .set('Accept', 'application/json')
+        .expect(405, done);
+    });
+
   });
 
 });
 
 function seed(done) {
   //seed the db
-  seedDb.seed(function(err, seedData){
+  seedDb.seed(function(err, seedData) {
     if (err) return done(err);
     done();
   });
