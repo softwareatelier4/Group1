0a3efefa0c60bd1602aac088ad7c9a9b9d1088ed
Susanna Riccardi <SusannaChuck@mbp-di-susanna.mobile.usilu.net>
2017-03-16 14:25:50 +0100

    Added search route. TODO tests.

--- routes/search/router.js
+++ routes/search/router.js
@@ -1,19 +1,44 @@
 /** @module search/router */
 'use strict';
 
-var express = require('express');
-var router = express.Router();
-var middleware =  require('../middleware');
-var rootUrl = require("../../config").url;
+const express = require('express');
+const router = express.Router();
+const middleware =  require('../middleware');
+const rootUrl = require("../../config").url;
+const utils = require('../utils');
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
+const Freelance = mongoose.model('Freelance');
 
-
-//supported methods
+// Supported methods
 router.all('/', middleware.supportedMethods('GET, OPTIONS'));
 
-//return index
+// Return index
 router.get('/', function(req, res, next) {
-  res.sendStatus(200);
+  const keyword = req.query.keyword;
+  const regex = new RegExp(keyword, "i");
+  const query = [
+    { firstName : regex },
+    { familyName : regex },
+    { title : regex },
+    { description : regex }
+  ]
+  Freelance.find({ $or : query }, function(err, results) {
+    if (err) {
+      res.status(400).json(err);
+    } else if (!results) {
+      res.status(404).json({
+        statusCode: 404,
+        message: "Not Found",
+      });
+    } else {
+      results.forEach(function(freelance) {
+        utils.addLinks(freelance, "freelance");
+      });
+      res.json(results).end();
+    }
+  });
 });
 
-/** router for root */
+/** router for search */
 module.exports = router;
