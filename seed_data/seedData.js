b851ce715c22c1b94d4d80850918b5171f593b68
Luca Dolfi <ldolfi@gmail.com>
2017-03-21 10:24:51 +0100

    modified location in range "Lugano || Mendiriso || Bellinzona"

--- seed_data/seedData.js
+++ seed_data/seedData.js
@@ -18,7 +18,7 @@ var catSize = categories.data.length;
 
 //set the category
 for (var i = freelancers.data.length - 1; i >= 0; i--) {
-  var n = Math.floor(Math.random()*catSize);
+  var n = Math.floor(Math.random() * catSize);
   // console.log(categories.data[n]['_id']);
   var rndId = categories.data[n]['_id'];
   freelancers.data[i]['category'] = rndId;
@@ -27,17 +27,23 @@ for (var i = freelancers.data.length - 1; i >= 0; i--) {
 // bind reviews
 for (var i = reviewsSize - 1; i >= 0; i--) {
   var id = reviews.data[i]['_id'];
-  var n = Math.floor(Math.random()*freelancersSize);
+  var n = Math.floor(Math.random() * freelancersSize);
   freelancers.data[n].reviews.push(id);
 }
 // bind tags
 for (var i = freelancersSize - 1; i >= 0; i--) {
-  var m = Math.floor(Math.random()*5 + 1); // give each freelancrs 1-5 random tags
+  var m = Math.floor(Math.random() * 5 + 1); // give each freelancrs 1-5 random tags
   for (var j = 0; j < m; j++) {
     var n = Math.floor(Math.random()*tagsSize);
     freelancers.data[i].tags.push(tags.data[n]['_id']);
     tags.data[n].freelancers.push(freelancers.data[i]['_id']);
   }
+}
+// hardcode the location
+var locations = ["Lugano", "Bellinzona", "Mendrisio"];
+for (var i = freelancers.data.length - 1; i >= 0; i--) {
+  var rndl = Math.floor(Math.random() * locations.length);
+  freelancers.data[i].address = locations[rndl];
 }
 // dump(freelancers.data);
 
