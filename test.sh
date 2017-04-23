0a73065f228edcf6622309f59d9fd1c664852989
falcon <aron.fiechter@gmail.com>
2017-04-23 22:16:36 +0200

    Check if "Unrecognized option: Spring" still occurs.

--- test.sh
+++ test.sh
@@ -7,5 +7,5 @@ printf "\nPROCESS USING PORT 3001:\n" && lsof -i:3000 || printf "NONE" && printf
 npm start &
 sleep 1
 node seed.js
-xvfb-run ./nightwatch
+npm run test-nightwatch-jenkins
 pkill "jobadvisor"
