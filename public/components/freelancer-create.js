52ca9dece5a75a69ebe9f263138c94da790867eb
Lara <brusel@usi.ch>
2017-04-23 15:28:13 +0200

    - Added claim form in "create as myself" - Made category field required

--- public/components/freelancer-create.js
+++ public/components/freelancer-create.js
@@ -13,45 +13,66 @@
  *   <label>Name</label>
  * </div>
  */
-function renderFreelancerTypeButtons() {
-  ReactDOM.render(
-    <FreelancerTypeButtons />,
-    document.getElementById('freelancer-root')
-  );
-}
 
-function renderComponent(data, isMyself) {
-  const categories = data;
-  const listCategories = categories.map((category, index) =>
-    <option key={index} value={category._id}>{category.categoryName}</option>
-  );
-
-  ReactDOM.render(
-   <CreationForm
-    categories={listCategories}
-    isMyself={isMyself}
-  />,
-   document.getElementById('freelancer-root')
-  );
-}
 
 class CreationForm extends React.Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.claim = this.claim.bind(this);
   }
 
   handleSubmit(evt) {
     evt.preventDefault();
 
+    // create freelancer
     const formData = {};
     for (const field in this.refs) {
       formData[field] = this.refs[field].value;
     }
 
+    let form = this;
+
     ajaxRequest("POST", "/freelance", {}, formData, function(data) {
-      window.location = "/freelance/" + data._id;
+      if(form.props.isMyself) {
+        // send claim request if registering myself
+        form.claim(data._id);
+      } else {
+        window.location = "/freelance/" + data._id;
+      }
+
     });
+  }
+
+  claim(createdFreelancerId) {
+    let files = document.getElementById('freelancer-claim-form-files').files;
+    if (files.length === 0) {
+      let message = document.getElementById('freelancer-claim-form-message');
+      message.innerHTML = 'No file was given';
+    } else {
+      ajaxRequest('POST', '/claim/new', { ajax : true }, { freelancerId : createdFreelancerId }, function(claim) {
+        if (claim._id) {
+          // Send files
+          let claimid = document.getElementById('freelancer-claim-form-claimid');
+          claimid.value = claim._id;
+
+          // Submit files
+          let formData = new FormData(document.getElementById('freelancer-claim-form-form'));
+          ajaxRequest('POST', '/claim/upload', null, formData, function(status) {
+            if(status == 202) {
+              // redirect to the created profile
+              window.location = "/freelance/" + createdFreelancerId;
+            } else {
+              console.log(status.error);
+            }
+          });
+
+        } else {
+          let message = document.getElementById('freelancer-claim-form-message');
+          message.innerHTML = 'Freelancer or user are already in a claim procedure, or you are not logged in';
+        }
+      });
+    }
   }
 
   render() {
@@ -93,8 +114,8 @@ class CreationForm extends React.Component {
             <label>
               Job Category
             </label>
-            <select name="category" ref="category">
-              <option selected disabled hidden>Please select a job category</option>
+            <select name="category" ref="category" required>
+              <option value="" selected disabled hidden>Please select a job category</option>
               {this.props.categories}
             </select>
           </div>
@@ -122,7 +143,7 @@ class CreationForm extends React.Component {
               Email
             </label>
           </div>
-
+          <div id="react-claim-form-root"></div>
           <input name="submit-button" className="submit-button" type="submit" value="Submit"/>
         </form>
       </div>
@@ -130,6 +151,69 @@ class CreationForm extends React.Component {
   }
 }
 
+/**
+ * Sends AJAX request and render the correct creation form
+ * @param  {Boolean} isMyself `true` if it should render the "Add myself as freelancer" form
+ */
+function doAjaxAndRenderForm(isMyself) {
+  ajaxRequest("GET",
+    "/category",
+    { ajax : true }, {},
+    function(data) { renderComponent(data, isMyself) }
+  );
+}
+
+/**
+ * Render form component as a result of AJAX
+ * @param  {Object}  data
+ * @param  {Boolean} isMyself
+ */
+function renderComponent(data, isMyself) {
+  const categories = data;
+  const listCategories = categories.map((category, index) =>
+    <option key={index} value={category._id}>{category.categoryName}</option>
+  );
+
+  ReactDOM.render(
+   <CreationForm
+    categories={listCategories}
+    isMyself={isMyself}
+  />,
+   document.getElementById('freelancer-root')
+  );
+
+  // render claiming form
+  if(isMyself) {
+    ReactDOM.render(
+      <FreelancerClaimForm />,
+      document.getElementById('react-claim-form-root')
+    );
+  }
+}
+
+/**
+ * Claim Form Component (same as freelancer page)
+ */
+class FreelancerClaimForm extends React.Component {
+  // clain() in CreationForm
+
+  render() {
+    return (
+      <div id="freelancer-claim-form">
+        <form id="freelancer-claim-form-form" encType="multipart/form-data" action="/claim/upload" method="post">
+          <input id="freelancer-claim-form-claimid" type="hidden" name="claimid" value=""/>
+          <input type="hidden" name="freelancerid" value={this.props.freelancerid} />
+          <input id="freelancer-claim-form-files" name="idfile" type="file" multiple="true" />
+        </form>
+        <div id="freelancer-claim-form-message"></div>
+      </div>
+    );
+  }
+}
+
+/**
+ * Form Type Buttons Component
+ */
 class FreelancerTypeButtons extends React.Component {
   constructor(props) {
     super(props);
@@ -147,12 +231,7 @@ class FreelancerTypeButtons extends React.Component {
       return;
     }
 
-    ajaxRequest("GET",
-      "/category",
-      { ajax : true }, {},
-      function(data) { renderComponent(data, isMyself) }
-    );
-
+    doAjaxAndRenderForm(isMyself);
   }
 
   render() {
@@ -165,8 +244,15 @@ class FreelancerTypeButtons extends React.Component {
   }
 }
 
-/**
- * Render components
- */
+function renderFreelancerTypeButtons() {
+  ReactDOM.render(
+    <FreelancerTypeButtons />,
+    document.getElementById('freelancer-root')
+  );
+}
 
+/**
+ * Render components function
+ */
+//TODO check url #myself
 renderFreelancerTypeButtons()
