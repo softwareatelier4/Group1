e1c279ee1358adda2a4fb867335d1afc4390d9c6
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2017-05-02 16:07:20 +0200

    Actually added route /freelance/edit

---
+++ public/components/manage-freelancer.js
@@ -0,0 +1,11 @@
+class FreelancerEditView extends React.Component {
+  render() {
+    return (
+      <div>TEST</div>
+    );
+  }
+}
+
+function renderPage() {
+  ReactDOM.render(<FreelancerEditView />, document.getElementById('react-freelancer-edit'));
+};
