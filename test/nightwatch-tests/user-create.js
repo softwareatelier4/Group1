a163f1ccf638ae2fc003f53e3497f0008e5d29f5
Yoshel <burgev@usi.ch>
2017-05-03 14:58:38 +0200

    Finished tests for user creation.

--- test/nightwatch-tests/user-create.js
+++ test/nightwatch-tests/user-create.js
@@ -11,6 +11,7 @@ module.exports = {
 
       // CORRECT: Registration as a new user
       // CORRECT: User being redirected to freelancer creation
+      // CORRECT: User being logged in after registration
       .click('button#user-register-btn')
       .waitForElementVisible('body', 1000)
       .waitForElementVisible('div#register-form', 1000)
@@ -26,31 +27,57 @@ module.exports = {
       .click('input[name=register-tick]')
       .click('input[name=submit-button]')
       .waitForElementVisible('body', 1000)
+      .waitForElementPresent('div#topbar', 1000)
+      .assert.containsText('div#react-username', 'nightwatch-user')
       .waitForElementPresent('div#freelancer-root', 10000)
       .assert.containsText('h1#freelancer-form-title', 'Create your own freelancer profile')
+      // logout
+      .click('button#freelancer-logout-btn')
+      .waitForElementVisible('body', 1000)
+      .waitForElementPresent('div#topbar', 1000)
+      .waitForElementPresent('button#user-register-btn', 1000)
 
-      // // add yourself
-      // .url( config.baseURL + '/freelance/new')
-      // // test alert if not logged in
-      // .click('button#freelancer-myself')
-      // .pause(1000)
-      // .acceptAlert()
-      // // login
-      // .setValue('input[name=login-username]', 'Bilbo')
-      // .setValue('input[name=login-password]', 'baggins')
-      // .click('input[name=login-submit]')
-      // .waitForElementPresent('button#freelancer-myself', 2000)
-      // .click('button#freelancer-myself')
-      // .assert.containsText('h1#freelancer-form-title', 'Create your own freelancer profile')
-      // .setValue('input[name=first-name]', 'Bilbo Baggins')
-      // .setValue('input[name=job-title]', 'Tester')
-      // .click('select[name="category"] option[value="58cc4b15fc13ae5ec7000123"]')
-      // .setValue('input[name=address]', 'USI, Lugano')
-      // .setValue('input[name=email]', 'test@bilbo.myself')
-      // .setValue('input#freelancer-claim-form-files', config.projectRoot + '/README.md')
-      // .click('input[name=submit-button]')
-      // .pause(2000)
-      // .assert.containsText('div#freelancer-claim-status-name', 'IN PROGRESS')
+      // CORRECT: User being redirected to home when the tick is not ticked
+      .click('button#user-register-btn')
+      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('div#register-form', 1000)
+      .assert.visible('input[name=username]')
+      .assert.visible('input[name=password]')
+      .assert.visible('input[name=confirm-password]')
+      .assert.visible('input[name=email]')
+      .setValue('input[name=username]', 'nightwatch-user2')
+      .setValue('input[name=password]', 'test')
+      .setValue('input[name=confirm-password]', 'test')
+      .setValue('input[name=email]', 'test@night.watch')
+      // do not click "Also create a freelancer profile for myself"
+      .click('input[name=submit-button]')
+      .waitForElementVisible('body', 1000)
+      .waitForElementPresent('div#topbar', 1000)
+      .assert.containsText('div#react-username', 'nightwatch-user2')
+      .waitForElementPresent('div#react-search-container', 10000)
+      .assert.visible('input[name=search-what]')
+      .assert.visible('input[name=search-where]')
+      // logout
+      .click('button#freelancer-logout-btn')
+      .waitForElementVisible('body', 1000)
+      .waitForElementPresent('div#topbar', 1000)
+      .waitForElementPresent('button#user-register-btn', 1000)
+
+      // ERROR: Registration with an already used username
+      .click('button#user-register-btn')
+      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('div#register-form', 1000)
+      .assert.visible('input[name=username]')
+      .assert.visible('input[name=password]')
+      .assert.visible('input[name=confirm-password]')
+      .assert.visible('input[name=email]')
+      .setValue('input[name=username]', 'nightwatch-user2')
+      .setValue('input[name=password]', 'test')
+      .setValue('input[name=confirm-password]', 'test')
+      .setValue('input[name=email]', 'test@night.watch')
+      .click('input[name=submit-button]')
+      // should still be on register form
+      .assert.visible('div#register-form')
       .end();
   }
 };
