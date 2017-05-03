fadd2d0020aff830fbfbf98708d817f9ba8b39d5
falcon <aron.fiechter@gmail.com>
2017-05-03 17:28:21 +0200

    Maybe nightwatch works now.

--- test.sh
+++ test.sh
@@ -7,5 +7,5 @@ printf "\nPROCESS USING PORT 3001:\n" && lsof -i:3000 || printf "NONE" && printf
 npm start &
 sleep 1
 node seed.js
-# xvfb-run -a -e /dev/stdout --  ./nightwatch # nightwatch runs successfully in local
+xvfb-run -a -e /dev/stdout --  ./nightwatch # nightwatch runs successfully in local
 pkill "jobadvisor"
