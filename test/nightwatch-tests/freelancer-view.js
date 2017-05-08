5d38f2d3474688d12911a21619323d7c9ea680a9
Jacopo Fidacaro <fidacj@usi.ch>
2017-05-08 10:49:04 +0200

    Tests for documents upload display.

--- test/nightwatch-tests/freelancer-view.js
+++ test/nightwatch-tests/freelancer-view.js
@@ -48,6 +48,9 @@ module.exports = {
       .click('button#freelancer-claim-btn')
       .pause(2000)
       .assert.containsText('div#freelancer-claim-form-message', 'Not enough required files submitted')
+      // display of correct document types
+      .assert.containsText('div#required-docs li:first-child', 'id')
+      .assert.containsText('div#optional-docs li:first-child', 'others')
       // Cannot do the following, since can't figure out how to 'upload' multiple required files with Nightwatch
       // .setValue('input#freelancer-claim-form-files', config.projectRoot + '/README.md')
       // .click('button#freelancer-claim-btn')
