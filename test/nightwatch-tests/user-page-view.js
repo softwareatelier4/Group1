60739ea5b3d5e5d3c89a86cf4b3123cb7569c43a
falcon <aron.fiechter@gmail.com>
2017-05-19 23:22:56 +0200

    Actually fixed.

--- test/nightwatch-tests/user-page-view.js
+++ test/nightwatch-tests/user-page-view.js
@@ -4,36 +4,34 @@ var config = require('../config');
 module.exports = {
   'User Page View' : function (client) {
     client
-    .useCss()
-
-    // Someone else's profile
-    .url( config.baseURL + '/user/Robb Bis')
-    .waitForElementVisible('body', 1000)
-    .waitForElementVisible('h1#user-page-title', 1000)
-    .assert.containsText('h1#user-page-title', 'Freelancer profiles owned by Robb Bis')
-    .waitForElementVisible('div.freelancer-card', 1000)
-    .assert.containsText('h1.job-title', 'Graphic Designer')
-    
-    // Login to see my user page
     .url( config.baseURL + '/')
+    .useCss()
     .waitForElementVisible('body', 1000)
     .waitForElementPresent('div#topbar', 1000)
     .waitForElementPresent('div#react-login', 3000)
-    .setValue('input[name=login-username]', 'Robb Bis')
-    .setValue('input[name=login-password]', 'perdonamierastil')
+    .setValue('input[name=login-username]', 'Dolben')
+    .setValue('input[name=login-password]', 'fooly')
     .click('input[name=login-submit]')
     .pause(1000)
     .waitForElementPresent('div#react-username a', 3000)
     .click('div#react-username a')
     .pause(1000)
-    
+
     // My own profile
     .waitForElementVisible('body', 1000)
     .waitForElementVisible('h1#user-page-title', 1000)
     .assert.containsText('h1#user-page-title', 'Freelancer profiles owned by you')
     .waitForElementVisible('div.freelancer-card', 1000)
     .assert.containsText('h1.job-title', 'Recruiting Manager')
-    
+
+    // Someone else's profile
+    .url( config.baseURL + '/user/Uial')
+    .waitForElementVisible('body', 1000)
+    .waitForElementVisible('h1#user-page-title', 1000)
+    .assert.containsText('h1#user-page-title', 'Freelancer profiles owned by Uial')
+    .waitForElementVisible('div.freelancer-card', 1000)
+    .assert.containsText('h1.job-title', 'Graphic Designer')
+
     .end();
   }
 };
