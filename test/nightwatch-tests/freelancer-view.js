38950c633d3f39aa3041292975bfbac457ba7d16
Lara <brusel@usi.ch>
2017-05-21 19:45:04 +0200

    brought back nice new working freelancer view tests

--- test/nightwatch-tests/freelancer-view.js
+++ test/nightwatch-tests/freelancer-view.js
@@ -64,24 +64,20 @@ module.exports = {
       .pause(500)
       // test empty review not listed
       .assert.hidden('div.review-text')
-      // duplicate form
-      .waitForElementPresent('button#freelancer-claim-toggle-duplicate', 1000)
-      // claim form
-      .waitForElementPresent('div#freelancer-claim', 1000)
-      // .assert.containsText('div#freelancer-claim-status-name', 'VERIFIED')
-      // .click('button#freelancer-claim-toggle-claim')
-      // .waitForElementPresent('div#freelancer-claim-form', 5000)
-      // .click('button#freelancer-claim-btn')
-      // .pause(2000)
-      // .assert.containsText('div#freelancer-claim-form-message', 'Not enough required files submitted')
-      // display of correct document types
-      // .assert.containsText('div#required-docs li:first-child', 'id')
-      // .assert.containsText('div#optional-docs li:last-child', 'other')
-      // Cannot do the following, since can't figure out how to 'upload' multiple required files with Nightwatch
-      //.setValue('input#freelancer-claim-form-files', config.projectRoot + '/README.md')
-      // .click('button#freelancer-claim-btn')
-      // .pause(2000)
-      // .assert.containsText('div#freelancer-claim-status-name', 'IN PROGRESS')
+      .click('button#freelancer-logout-btn')
+      .pause(500)
+      .url( config.baseURL + '/freelance/58cc4941fc13ae612c00000f')
+      .pause(500)
+      .setValue('input[name=login-username]', 'Robb Bis')
+      .setValue('input[name=login-password]', 'perdonamierastil')
+      .click('input[name=login-submit]')
+      .pause(500)
+      .waitForElementPresent('button#freelancer-edit-button', 1000)
+      .waitForElementPresent('button#freelancer-delete-button', 1000)
+      .click('button#freelancer-delete-button') // cancel delete
+      .dismissAlert()
+      .click('button#freelancer-edit-button') // cancel delete
+      .waitForElementPresent('#edit-btn-schedule', 1000)
       .end();
   }
 };
