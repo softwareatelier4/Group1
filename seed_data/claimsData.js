d045e00bc0500cf05b2d5bcc3e53726f468e106c
Luca Dolfi <ldolfi@gmail.com>
2017-05-28 23:36:50 +0200

    readded fake claims, made them optionals in demo

--- seed_data/claimsData.js
+++ seed_data/claimsData.js
@@ -2,6 +2,9 @@ var claimsData = {
   name: 'Claim',
   data: [
     // The following claims are all wrong, for test purposes
+    {_id:"58cc4941fc13ae612c20000a",freelanceID:"000000000000000000000000",userID:"000000000000000000000000"},
+    {_id:"58cc4941fc13ae612c20000b",freelanceID:"58cc4941fc13ae612c00000a",userID:"000000000000000000000000"},
+    {_id:"58cc4941fc13ae612c20000c",freelanceID:"58cc4941fc13ae612c00000e",userID:"000000000000000000000000"},
     {_id:"58cc4941fc13ae612c20000d",freelanceID:"58cc4941fc13ae612c00000e",userID:"58cc4941fc13ae612a10000a"},
     {_id:"58cc4941fc13ae612c20000e",freelanceID:"58cc4941fc13ae612c00000e",userID:"58cc4941fc13ae612a10000b"},
     {_id:"58cc4941fc13ae612c20000f",freelanceID:"58cc4942fc13ae612c000043",userID:"58cc4941fc13ae612a10000a"}
